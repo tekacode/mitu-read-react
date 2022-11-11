f23c49b7224f74f1486cb72696d29df8b3b57438
Tekalegn Fisseha <objectp@yahoo.com>
2022-11-11 16:44:19 -0500

    Updates

---
+++ precache-manifest.e6994c2b9d56da4914477f8c7154038d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d74d7d1ae4d39255fd6",
+    "url": "/mitu-read-react/static/css/main.12bcc39d.chunk.css"
+  },
+  {
+    "revision": "9d74d7d1ae4d39255fd6",
+    "url": "/mitu-read-react/static/js/main.9d74d7d1.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/mitu-read-react/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "7587d2c9267632307ee0",
+    "url": "/mitu-read-react/static/js/2.7587d2c9.chunk.js"
+  },
+  {
+    "revision": "eeab3377fb71d64c3aa6",
+    "url": "/mitu-read-react/static/js/runtime~main.eeab3377.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/mitu-read-react/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9966b31aa49ae5d6037030ae98eddb6b",
+    "url": "/mitu-read-react/index.html"
+  }
+];
